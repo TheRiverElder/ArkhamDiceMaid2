e28ce799623a57490235d9d8e40edbb9e02ad2ff
TheRiverElder <765375399@qq.com>
2020-04-07 14:39:51 +0800

    add action for Command_Control

--- top.riverelder.arkham.Code/Commands/Command_Control.cs
+++ top.riverelder.arkham.Code/Commands/Command_Control.cs
@@ -20,7 +20,23 @@ namespace top.riverelder.arkham.Code.Commands {
                 PresetNodes.String<DMEnv>("卡名")
                 .Handles(Extensions.ExistInv())
                 .Executes((env, args, dict) => Control(env.SelfId, env.Sce, args.GetInv("卡名")))
+                .Then(
+                    PresetNodes.Rest<DMEnv>("行动").Executes((env, args, dict) => ControlAndAct(env, args.GetInv("卡名"), args.GetStr("行动")))
+                )
             );
+        }
+
+        public static string ControlAndAct(DMEnv env, Investigator inv, string action) {
+            Scenario sce = env.Sce;
+            if (!sce.player2investigatorMap.TryGetValue(env.SelfId, out string selfName)) {
+                selfName = null;
+            }
+            sce.Control(env.SelfId, inv.Name);
+            Global.Dispatcher.Dispatch(action, env, out string reply);
+            if (selfName != null) {
+                sce.Control(env.SelfId, selfName);
+            }
+            return $"以{inv.Name}的身份：\n" + reply;
         }
     }
 }
