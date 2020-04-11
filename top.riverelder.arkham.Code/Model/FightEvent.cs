a1087d7059c930fd850e8187bccc0edd58b7d048
TheRiverElder <765375399@qq.com>
2020-04-11 20:00:11 +0800

    add Spell

--- top.riverelder.arkham.Code/Model/FightEvent.cs
+++ top.riverelder.arkham.Code/Model/FightEvent.cs
@@ -16,4 +16,64 @@ namespace top.riverelder.arkham.Code.Model {
             WeaponName = weaponName;
         }
     }
+
+
+    public class FightEvent1 {
+
+        // 战斗顺序
+        public List<string> Order = new List<string>();
+
+        // 声明延迟行动的调查员
+        public HashSet<string> Delayed = new HashSet<string>();
+
+        public int Index = 0;
+
+        public string GetNextName() {
+            return Order[Index];
+        }
+
+        public string TurnToNext() {
+            if (++Index > Order.Count) {
+                Index = 0;
+                Delayed.Clear();
+            }
+            return Index < Order.Count ? Order[Index] : null;
+        }
+
+        public string GetWhoCanActHint() {
+            StringBuilder builder = new StringBuilder();
+            if (Index >= Order.Count) {
+                builder.Append("本轮已经结束，");
+                if (Delayed.Count > 0) {
+                    builder.AppendLine("仅申明延迟者可行动：").Append(string.Join("，", Delayed)); 
+                } else {
+                    builder.Append("无人申明延迟，请开始新的一轮");
+                }
+            } else {
+                builder.Append("下一个行动者：").Append(Order[Index]);
+                if (Delayed.Count > 0) {
+                    builder.AppendLine("，或申明延迟者：").Append(string.Join("，", Delayed));
+                } else {
+                    builder.Append("，无人申明延迟");
+                }
+            }
+            return builder.ToString();
+        }
+
+        public bool CanAct(string name, out string err) {
+            if (!Order.Contains(name)) {
+                err = name + "没有参加战斗";
+                return false;
+            } else if (Delayed.Contains(name)) {
+                err = null;
+                return true;
+            } else if (GetNextName() == name) {
+                err = null;
+                return true;
+            } else {
+                err = $"未轮到{name}行动，" + GetWhoCanActHint();
+                return false;
+            }
+        }
+    }
 }
---
+++ top.riverelder.arkham.Code/Model/Spell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using top.riverelder.arkham.Code.Utils;
+
+namespace top.riverelder.arkham.Code.Model {
+    public class Spell {
+
+        public string Name { get; }
+
+        public Dictionary<string, string> Cost { get; }
+
+        public Spell(string name, Dictionary<string, string> cost) {
+            Name = name;
+            Cost = cost;
+        }
+
+        public bool Use(Investigator user, out string reply) {
+            Dictionary<string, int> actualCost = new Dictionary<string, int>();
+            foreach (var e in Cost) {
+                int c = Dice.RollWith(e.Value, user.DamageBonus);
+                if (!user.Values.TryWidelyGet(e.Key, out Value value)) {
+                    reply = user.Name + "不存在数值：" + e.Key;
+                    return false;
+                } else if (value.Val < c) {
+                    reply = $"{user.Name}的{e.Key}少于消耗数值：{value.Val} < {c}";
+                    return false;
+                }
+                actualCost[e.Key] = c;
+            }
+            StringBuilder builder = new StringBuilder();
+            builder.Append("消耗");
+            foreach (var e in actualCost) {
+                if (!user.Values.TryWidelyGet(e.Key, out Value value)) {
+                    reply = user.Name + "不存在数值：" + e.Key;
+                    return false;
+                }
+                int prev = value.Val;
+                value.Sub(e.Value);
+                builder.AppendLine().Append(e.Key).Append('：').Append(prev).Append(" - ").Append(e.Value).Append(" => ").Append(value.Val);
+            }
+            reply = builder.ToString();
+            return true;
+        }
+    }
+}
