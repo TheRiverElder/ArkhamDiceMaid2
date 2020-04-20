d6f8572cf2698634331a2a40e8b0a093808eff4a
TheRiverElder <765375399@qq.com>
2020-04-21 00:44:31 +0800

    fix SetDB in Command_Inv

--- top.riverelder.arkham.Code/Commands/Command_Inv.cs
+++ top.riverelder.arkham.Code/Commands/Command_Inv.cs
@@ -82,22 +82,22 @@ namespace top.riverelder.arkham.Code.Commands {
                         .MapDict(mapper)
                         .Executes((env, args, dict) => CreateInv(env, args.GetStr("名称"), args.GetStr("描述"), dict))
                     )
-                ).Then(
-                    PresetNodes.Literal<DMEnv>("重算")
-                    .Handles(Extensions.ExistSelfInv())
-                    .Executes((env, args, dict) => ReCalc(env, env.Inv))
-                    .Then(
-                        PresetNodes.String<DMEnv>("名称")
-                        .Handles(Extensions.ExistInv())
-                        .Executes((env, args, dict) => ReCalc(env, args.GetInv("名称")))
-                    )
-                ).Then(
-                    PresetNodes.Literal<DMEnv>("伤害加值")
-                    .Then(
-                        Extensions.Dice("数值")
-                        .Handles(Extensions.ExistSelfInv())
-                        .Executes((env, args, dict) => SetDB(env, env.Inv, args.GetDice("数值")))
-                    )
+                )
+            ).Then(
+                PresetNodes.Literal<DMEnv>("重算")
+                .Handles(Extensions.ExistSelfInv())
+                .Executes((env, args, dict) => ReCalc(env, env.Inv))
+                .Then(
+                    PresetNodes.String<DMEnv>("名称")
+                    .Handles(Extensions.ExistInv())
+                    .Executes((env, args, dict) => ReCalc(env, args.GetInv("名称")))
+                )
+            ).Then(
+                PresetNodes.Literal<DMEnv>("伤害加值")
+                .Handles(Extensions.ExistSelfInv())
+                .Then(
+                    Extensions.Dice("数值")
+                    .Executes((env, args, dict) => SetDB(env, env.Inv, args.GetDice("数值")))
                 )
             );
 
