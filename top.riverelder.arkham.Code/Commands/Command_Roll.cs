9cef78591d3821db9ca1e1a9c722996821799859
TheRiverElder <765375399@qq.com>
2020-04-22 14:48:49 +0800

    add choose function for Command_Roll

--- top.riverelder.arkham.Code/Commands/Command_Roll.cs
+++ top.riverelder.arkham.Code/Commands/Command_Roll.cs
@@ -15,14 +15,29 @@ namespace top.riverelder.arkham.Code.Commands {
         public string Usage => "投掷 <骰子>";
 
         public override void OnRegister(CmdDispatcher<DMEnv> dispatcher) {
-            dispatcher.Register("投掷")
-                .Then(
-                    Extensions.Dice("骰子").Executes((env, args, dict) => Roll(args.GetDice("骰子")))
-                );
+            dispatcher.Register("投掷").Then(
+                Extensions.Dice("骰子").Executes((env, args, dict) => Roll(args.GetDice("骰子")))
+            ).Rest(
+                Extensions.Dice("选项组")
+                .Handles(Extensions.ConvertObjectArrayToStringArray())
+                .Executes((env, args, dict) => Choose(args.Get<string[]>("选项组")))
+            );
         }
 
         public static string Roll(Dice dice) {
             return $"{dice.ToString()} = {dice.Roll()}";
+        }
+
+        public static string Choose(string[] results) {
+            if (results.Length > 0) {
+                int index = Dice.Roll("1d" + results.Length);
+                if (index >= 0 && index < results.Length) {
+                    return results[index];
+                } else {
+                    return "骰娘出错了๐·°(৹˃̵﹏˂̵৹)°·๐";
+                }
+            }
+            return "没有选项哦";
         }
     }
 }
