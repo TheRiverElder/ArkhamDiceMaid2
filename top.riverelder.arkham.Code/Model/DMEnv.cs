0ed469a87704ba96d5e37bb7a3c5b83cf5b90a7b
TheRiverElder <765375399@qq.com>
2020-05-21 22:27:41 +0800

    use DiceException

--- top.riverelder.arkham.Code/Model/DMEnv.cs
+++ top.riverelder.arkham.Code/Model/DMEnv.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using top.riverelder.arkham.Code.Exceptions;
 using top.riverelder.arkham.Code.Utils;
 
 namespace top.riverelder.arkham.Code.Model {
@@ -83,15 +84,15 @@ namespace top.riverelder.arkham.Code.Model {
                     return SceCache;
                 }
                 if (!Global.Groups.TryGetValue(GroupId, out string sceName)) {
-                    throw new Exception("该群还未关联存档！");
+                    throw new DiceException("该群还未关联存档！");
                 }
                 if (!Global.Scenarios.TryGetValue(sceName, out Scenario sce)) {
                     if (Global.AutoLoad) {
                         if (!SaveUtil.TryLoad(sceName, out sce)) {
-                            throw new Exception($"该群所关联的存档【{sceName}】自动载入失败！");
+                            throw new DiceException($"该群所关联的存档【{sceName}】自动载入失败！");
                         }
                     } else {
-                        throw new Exception($"该群所关联的存档【{sceName}】还未载入！\n使用“读团”指令或者配置自动载入存档");
+                        throw new DiceException($"该群所关联的存档【{sceName}】还未载入！\n使用“读团”指令或者配置自动载入存档");
                     }
                 }
                 return SceCache = sce;
@@ -105,10 +106,10 @@ namespace top.riverelder.arkham.Code.Model {
                 }
                 Scenario sce = Sce;
                 if (!Sce.PlayerNames.TryGetValue(SelfId, out string invName)) {
-                    throw new Exception("你还未与人物卡关联");
+                    throw new DiceException("你还未与人物卡关联");
                 }
                 if (!sce.Investigators.TryGetValue(invName, out Investigator inv)) {
-                    throw new Exception($"不存在名为【{invName}】的人物卡，该卡可能已经被删除！");
+                    throw new DiceException($"不存在名为【{invName}】的人物卡，该卡可能已经被删除！");
                 }
                 return InvCache = inv;
             }
